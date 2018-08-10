54801bd4434f7e61d5e422cf71d523d89e9adc01
Flowaria <wnsghgj1234@naver.com>
2018-08-11 02:03:43 +0900

    초기 커밋.

---
+++ LanotaliumPM/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LanotaliumPM
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            foreach (var file in Directory.GetFiles("/StreamingAssets/Plugins"))
+            {
+                Assembly assembly = Assembly.LoadFrom(file);
+                Type[] types = assembly.GetTypes();
+                foreach (var type in types)
+                {
+                    //Creator Buttons
+                    if (type.GetInterface("ILanotaliumPlugin") != null)
+                    {
+                        
+                    }
+                }
+            }
+            InitializeComponent();
+        }
+    }
+}
